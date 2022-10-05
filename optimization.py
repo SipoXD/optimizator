cb9f48a7b2b4f7a818cdfeeee22575bdeb7e4539
Sipo <78769530+SipoXD@users.noreply.github.com>
2022-10-04 20:08:53 -0600

    Add files via upload

---
+++ optimization.py
@@ -0,0 +1,28 @@
+import os, ctypes, shutil
+
+windows_user = os.getenv("username")
+
+def isAdmin():
+    try:
+        is_admin = (os.getuid() == 0)
+    except AttributeError:
+        is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+    return is_admin
+
+if isAdmin() == False:
+    exit()
+
+try:
+    os.system("sc stop sysmain")
+    os.system("sc stop diagtrack")  
+    shutil.rmtree(r"C:\\Users\\" + windows_user + r"\\AppData\\Local\\Temp", ignore_errors=True)
+    shutil.rmtree(r"C:\Windows\\Prefetch", ignore_errors=True)
+    os.system("cls")
+    print("Succesfully stopped diagtrack")
+    print("Succesfully stopped sysmain")
+    print("Succesfully deleted prefetch")
+    print("Succesfully deleted %temp%")
+    input("Press enter to exit...")
+except:
+    os.system("cls")
+    input("Failed, press enter to exit...")
